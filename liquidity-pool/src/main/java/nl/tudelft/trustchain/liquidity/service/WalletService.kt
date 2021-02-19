af1f6fc05c850cf357bcb7bb687516512f029201
Chris Lemaire <c.lemaire@student.tudelft.nl>
2021-02-19 15:16:47 +0100

    Add liquidity pool and wallet classes

--- liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/service/WalletService.kt
+++ liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/service/WalletService.kt
@@ -1,4 +1,34 @@
 package nl.tudelft.trustchain.liquidity.service
 
-class WalletService {
+import android.content.ContextWrapper.*
+import org.bitcoinj.core.ECKey
+import org.bitcoinj.kits.WalletAppKit
+import org.bitcoinj.params.RegTestParams
+import org.bitcoinj.wallet.Wallet
+import java.io.File
+import java.net.URL
+
+object WalletService {
+    private const val bitcoinFaucetEndpoint = "http://134.122.59.107:3000"
+    val params = RegTestParams.get()
+
+    fun createPersonalWallet(dir: File): Wallet =
+        createWallet(dir, "personal")
+
+    fun createMultiSigWallet(dir: File): Wallet =
+        createWallet(dir, "multi-sig")
+
+    fun createWallet(dir: File, name: String): Wallet =
+        object : WalletAppKit(params, dir, name) {
+            override fun onSetupCompleted() {
+                if (wallet().keyChainGroupSize < 1) {
+                    wallet().importKey(ECKey())
+                }
+
+                if (wallet().balance.isZero) {
+                    val address = wallet().issuedReceiveAddresses.first().toString()
+                    URL("$bitcoinFaucetEndpoint?id=$address").readBytes()
+                }
+            }
+        }.wallet()
 }
