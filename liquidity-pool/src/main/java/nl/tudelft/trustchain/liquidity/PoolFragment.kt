3ec63f942b812e89de0ae387fc3c42c1d606a540
vstam <just.van.stam@gmail.com>
2021-02-28 11:08:53 +0100

    update design liquidity pool

--- liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/PoolFragment.kt
+++ liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/PoolFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [PoolFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PoolFragment : BaseFragment(R.layout.fragment_pool) {
+class PoolFragment : BaseFragment(R.layout.fragment_pool_new) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class PoolFragment : BaseFragment(R.layout.fragment_pool) {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pool, container, false)
+        return inflater.inflate(R.layout.fragment_pool_new, container, false)
     }
 
     companion object {
