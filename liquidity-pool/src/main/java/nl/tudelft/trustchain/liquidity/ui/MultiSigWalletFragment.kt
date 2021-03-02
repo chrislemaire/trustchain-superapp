ab622e6d47ef4bb40a917a7f2b5ede8a65399509
thversfelt <thversfelt@hotmail.com>
2021-03-02 08:06:00 +0100

    Made multisig fragment depend on the trustchain base fragment

--- liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/ui/MultiSigWalletFragment.kt
+++ liquidity-pool/src/main/java/nl/tudelft/trustchain/liquidity/ui/MultiSigWalletFragment.kt
@@ -1,13 +1,13 @@
 package nl.tudelft.trustchain.liquidity.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import nl.tudelft.trustchain.common.ui.BaseFragment
 import nl.tudelft.trustchain.liquidity.R
 
-class MultiSigWalletFragment : Fragment() {
+class MultiSigWalletFragment : BaseFragment(R.layout.fragment_pool_multi_sig_wallet) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
